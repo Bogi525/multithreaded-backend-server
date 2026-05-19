dcffd21f3070ed8cddce7561b4e89b5160596622
Bogi525 <bogithe525@gmail.com>
2026-05-19 19:11:12 +0200

    initial commit

---
+++ inc/server.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Server {
+public:
+    void funct();
+private:
+    int a = 5;
+};
---
+++ src/server.cpp
@@ -0,0 +1,6 @@
+#include "../inc/server.hpp"
+
+#include <sys/socket.h>
+
+void Server::funct() {
+}
